1a519b3d3f03ae393d0d421301145fd8e592b477
tipa1488amerikos <tipa1488amerikos@gmail.com>
2021-02-13 12:19:44 +0200

    spa configure

--- BrainenceExchange/BrainenceExchange/Startup.cs
+++ BrainenceExchange/BrainenceExchange/Startup.cs
@@ -1,39 +1,121 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace BrainenceExchange
 {
     public class Startup
     {
+         private IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder =>
+                    {
+                        builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                    });
+            });
+
+            services
+                .AddMvc(
+                options =>
+                {
+                    options.EnableEndpointRouting = false;
+                    // options.Filters.Add(new ValidationFilter());
+                }
+                )
+                .AddFluentValidation(
+                options =>
+                {
+                    options.RegisterValidatorsFromAssemblyContaining<Startup>();
+                }
+                );
+
+            
+
+            // services.AddDbContext<ApplicationDbContext>(options =>
+            //     options.UseNpgsql(Configuration["ConnectionStrings:MainDB"]));
+            // services.AddTransient<IDataRepository, PsqlDataRepository>();
+ 
+            // var jwtSection = Configuration.GetSection("JwtBearerTokenSettings");
+            // services.Configure<JwtBearerTokenSettings>(jwtSection);
+            
+           
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddHttpClient();
+
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "frontend/build";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app,
+                             IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            else
+            {
+                app.UseExceptionHandler("/error");
+            }
+            app.UseSpaStaticFiles();
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+
+
+            // app.UseAuthentication();
+            // app.UseAuthorization();
+
+
+            app.UseCors(builder =>
+            builder
+            .WithOrigins("https://localhost:5001", "https://localhost:3000", "http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+            );
+
+            app.UseMvc();
+
+            app.UseSpa(spa =>
             {
-                endpoints.MapGet("/", async context =>
+                spa.Options.SourcePath = "frontend";
+
+                if (env.IsDevelopment())
                 {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
             });
         }
     }
