40dca6ece7ae06b38099d4022641a5f103ffd49c
tipa1488amerikos <tipa1488amerikos@gmail.com>
2021-02-15 09:34:23 +0200

    utils improvements

--- BrainenceExchange/BrainenceExchange/frontend/src/utils/endpoints.js
+++ BrainenceExchange/BrainenceExchange/frontend/src/utils/endpoints.js
@@ -1,4 +1,4 @@
-export const getPictureByCode = 'https://localhost:5001/api/Pictures/Get/';
-export const getExchange = 'https://localhost:5001/api/Exchange';
-export const getAvailableCurrency = 'https://localhost:5001/api/Info/AvailableCurrency';
-export const getHistory = 'https://localhost:5001/api/History';
+export const getPictureByCode = '/api/Pictures/Get/';
+export const getExchange = '/api/Exchange';
+export const getAvailableCurrency = '/api/Info/AvailableCurrency';
+export const getHistory = '/api/History';
